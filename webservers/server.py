14bd0c291164a32df22b0207c42b59f35b667e06
Matt Gerst <mattgerst@gmail.com>
2017-02-21 14:06:11 -0600

    Add cookies and other headers examples

--- webservers/server.py
+++ webservers/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from bottle import Bottle, run, request, auth_basic
+from bottle import Bottle, run, request, auth_basic, response, abort
 
 app = Bottle()
 
@@ -7,13 +7,13 @@ app = Bottle()
 @app.route('/ua')
 def ua():
     print(request.headers.get('User-Agent'))
-    if request.headers.get('User-Agent') != 'python/requests':
+    if request.headers.get('User-Agent') != 'python-scraper/1.0':
         return "I don't like your browser, go away!"
     return "Hisss."
 
 
 def check_auth(username, password):
-    if username == 'admin' and password == '12308':
+    if username == 'admin' and password == '308':
         return True
     return False
 
@@ -21,7 +21,32 @@ def check_auth(username, password):
 @app.route('/basic_auth')
 @auth_basic(check_auth)
 def basic():
-    pass
+    return "Welcome authenticated user!"
+
+
+@app.route('/token')
+def token():
+    auth = request.get_header('Authorization')
+    if auth:
+        method, value = auth.split(' ')
+
+        if method == "Token" and value == "142":
+            return "Success!"
+
+    abort(401, "Unauthorized")
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    response.set_cookie("authenticated", "yes")
+
+
+@app.route('/cookie')
+def cook():
+    if request.get_cookie("authenticated") == "yes":
+        return "You are a valid user"
+    else:
+        abort(401, "Access denied")
 
 
 if __name__ == "__main__":
